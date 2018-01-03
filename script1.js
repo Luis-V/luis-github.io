1d390615225765ae2c9595078fec81b50a37e7b5
Luis-V <31395374+Luis-V@users.noreply.github.com>
2018-01-02 20:39:55 -0500

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,4 @@
+//this document defines the placeholders for the fields in the contact form
+document.getElementsByTagName("input")[0].placeholder = "Your name. For example: Jessica"; 
+document.getElementsByTagName("input")[1].placeholder = "Your last name. For example: Gilfillan";
+document.getElementsByTagName("textarea")[0].placeholder = "Write questions, comments, insights, complaints, insults, jokes, or anything."; 
